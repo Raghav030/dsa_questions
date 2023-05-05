ae789d938544ae1a0bf2aedd6eb6645cf2aefa87
Raghav030 <106468954+Raghav030@users.noreply.github.com>
2023-05-05 23:46:46 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -7,32 +7,46 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	int fparent (int node, vector<int>& parent){
+	    if (parent[node]==node) return node;
+	    return parent[node]=fparent(parent[node], parent);
+	}
+	void findunion(int weight, int n1, int n2, vector <int>& parent, vector<int>& rank, int& ans){
+	    int pn1=fparent(n1, parent);
+	    int pn2=fparent(n2, parent);
+	    if (pn1==pn2) return;
+	    ans+=weight;
+	    if (rank[n1]<rank[n2]){
+	        parent[pn1]=pn2;
+	    }
+	    else if (rank[n1]>rank[n2]){
+	        parent[pn2]=pn1;
+	        
+	    }
+	    else {
+	        parent[pn2]=pn1;
+	        rank[pn1]+=1;
+	    }
+	}
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        // vector <vector<pair<int,int>>> graph(V);
-        // for (int i=0; i<adj.length(); i++){
-        //     graph[adj[i][0]].push_back({adj[i][1],adj[i][2]});
-        //     graph[adj[i][1]].push_back({adj[i][0],adj[i][2]});
-        // }
-        vector <int> vis(V,0);
-        int ans=0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,adj[0][0][0]});
-        while (!pq.empty()){
-            int node=pq.top().second;
-            int distance=pq.top().first;
-            pq.pop();
-            if (vis[node]==0){
-                vis[node]=1;
-                ans+=distance;
-                for (int i=0; i<adj[node].size(); i++){
-                    int adjnode=adj[node][i][0];
-                    if (vis[adjnode] != 1){
-                        pq.push({adj[node][i][1], adjnode});
-                    }
-                }
+        // priority_queue <pair<int,pair<int,int>>, vector<int,pair<int,int>>, greater<int,pair<int,int>>> pq;
+        vector <pair<int, pair<int, int>>> nodes;
+        for (int i=0; i<V; i++){
+            for (int j=0; j<adj[i].size(); j++){
+                nodes.push_back({adj[i][j][1], {i, adj[i][j][0]}});
             }
+        }
+        int ans=0;
+        sort(nodes.begin(), nodes.end());
+        vector <int> parent(V+1,0);
+        vector <int> rank(V+1,0);
+        for (int i=0; i<parent.size(); i++){
+            parent[i]=i;
+        }
+        for (int i=0; i<nodes.size(); i++){
+            findunion(nodes[i].first, nodes[i].second.first, nodes[i].second.second,parent , rank, ans);
         }
         return ans;
     }
