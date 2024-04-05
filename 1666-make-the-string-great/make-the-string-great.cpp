a78f717ead39c08d6160bd41e2fb7ba35418cb6c
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-04-05 23:05:08 +0530

    Time: 2 ms (58.69%) | Memory: 7.5 MB (89.71%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        // string a="ssZDDVDVF";
+        int n=s.size(), i=0;
+        while (s.size()>0 and i<s.size()-1){
+            if (s[i]+32==s[i+1] || s[i+1]+32== s[i]){
+                s.erase(i,2);
+                if (i>0) i-=1;
+            }
+            else i+=1;
+        }
+        return s;
+    }
+};
