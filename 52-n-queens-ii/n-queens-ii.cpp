49c75edb3678e0428591c85f5975ec793f33de09
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-04-15 23:19:43 +0530

    Time: 5 ms (37.25%) | Memory: 7.2 MB (83.21%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkposition(int row, int col, vector<string>& vect, int n){
+        for (int i=0; i<n; i++){
+            if (vect[i][col]=='Q') return false;
+        }
+        int i=row, j=col;
+        while (i>=0 and j>=0 and i<n and j<n){
+            if (vect[i][j]=='Q') return false;
+            i-=1;
+            j-=1;
+        }
+        i=row, j=col;
+        while (i>=0 and j>=0 and i<n and j<n){
+            if (vect[i][j]=='Q') return false;
+            i-=1;
+            j+=1;
+        }
+        return true;
+    }
+    void fnqueen(int n, int row, int col,vector<string>& vect,  int& ans){
+        if (row== n){
+            ans+=1;
+        }
+        for (int i=0; i<n; i++){
+            if (checkposition(row,i, vect, n)==true){
+                vect[row][i]='Q';
+                fnqueen(n, row+1, i, vect, ans);
+                vect[row][i]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        int ans=0;
+        vector<string> vect(n, string(n, '.'));
+        fnqueen(n, 0, 0,vect, ans);
+        return ans;
+    }
+};
