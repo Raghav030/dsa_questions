729ddcc45d8b0ec7386b3a8592480316174d7498
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-04-15 22:43:55 +0530

    Time: 4 ms (63.31%) | Memory: 9.3 MB (81.58%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,37 +1,81 @@
 class Solution {
 public:
-    std::vector<std::vector<std::string> > solveNQueens(int n) {
-        std::vector<std::vector<std::string> > res;
-        std::vector<std::string> nQueens(n, std::string(n, '.'));
-        solveNQueens(res, nQueens, 0, n);
-        return res;
+    bool checkposition(vector<string>& vect, int row, int col, int n){
+        for (int i=0; i<n; i++){
+            if (vect[row][i]=='Q') return false;
+        }
+        for (int i=0; i<n; i++){
+            if (vect[i][col]=='Q') return false;
+        }
+        int i=row, j=col;
+        while (i>=0 and j>=0 and i<n and j<n){
+            if (vect[i][j]=='Q') return false;
+            i+=1;
+            j-=1;
+        }
+        i=row, j=col;
+        while (i>=0 and j>=0 and i<n and j<n){
+            if (vect[i][j]=='Q') return false;
+            i+=1;
+            j+=1;
+        }
+        i=row, j=col;
+        while (i>=0 and j>=0 and i<n and j<n){
+            if (vect[i][j]=='Q') return false;
+            i-=1;
+            j-=1;
+        }
+        i=row, j=col;
+        while (i>=0 and j>=0 and i<n and j<n){
+            if (vect[i][j]=='Q') return false;
+            i-=1;
+            j+=1;
+        }
+        return true;
     }
-private:
-    void solveNQueens(std::vector<std::vector<std::string> > &res, std::vector<std::string> &nQueens, int row, int &n) {
-        if (row == n) {
-            res.push_back(nQueens);
+    void fnqueens(int n, int row, int col, vector<string>& vect, vector<vector<string>>& ans){
+        if (n==row){
+            ans.push_back(vect);
             return;
         }
-        for (int col = 0; col != n; ++col)
-            if (isValid(nQueens, row, col, n)) {
-                nQueens[row][col] = 'Q';
-                solveNQueens(res, nQueens, row + 1, n);
-                nQueens[row][col] = '.';
+        for (int i=0; i<n; i++){
+            if (checkposition(vect, row, i, n)==true){
+                vect[row][i]='Q';
+                fnqueens(n, row+1,col, vect, ans);
+                vect[row][i]='.';
             }
+        }
     }
-    bool isValid(std::vector<std::string> &nQueens, int row, int col, int &n) {
-        //check if the column had a queen before.
-        for (int i = 0; i != row; ++i)
-            if (nQueens[i][col] == 'Q')
-                return false;
-        //check if the 45° diagonal had a queen before.
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
-            if (nQueens[i][j] == 'Q')
-                return false;
-        //check if the 135° diagonal had a queen before.
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
-            if (nQueens[i][j] == 'Q')
-                return false;
-        return true;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> vect(n,string(n, '.'));
+        // for (int i=0; i<n; i++){
+        //     vector<string> vect(n,string(n, '.'));
+        //     // for (int i=0; i<n; i++){
+        //     //     for (int j=0; j<n; j++){
+        //     //         cout<< vect[i]<<' ';
+        //     //     }
+        //     //     cout<< endl;
+        //     // }
+        //     vect[0][i]='Q';
+        //     int flag=0;
+        //     for (int k=1; k<n; k++){
+        //         flag=0;
+        //         for (int j=0; j<n; j++){
+        //             if (checkposition(vect, k, j, n)){
+        //                 vect[k][j]='Q';
+        //                 flag=1;
+        //                 break;
+        //             }
+        //         }
+        //         if (flag==0) break;
+        //     }
+        //     if (flag==1 or n==1){
+        //         ans.push_back(vect);
+        //     }
+        // }
+        fnqueens(n, 0, 0, vect, ans);
+        return ans;
+        
     }
 };
