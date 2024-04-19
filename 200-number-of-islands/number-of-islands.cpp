885bc72ba9eecadbe97599d8ec1b585d4f7566d1
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-04-19 23:58:15 +0530

    Time: 26 ms (77.13%) | Memory: 23.4 MB (21.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -4,29 +4,30 @@ public:
         vector<int> rvect={0, -1, 0, 1};
         vector<int> cvect= {1, 0, -1, 0};
         grid[row][col]='2';
-        // queue<pair<int, int>> q;
-        // q.push({row, col});
-        // while (q.size()>0){
-        //     row=q.front().first;
-        //     col= q.front().second;
-        //     q.pop();
-        //     grid[row][col]='2';
-        //     for(int i=0; i<4; i++){
-        //         int rnext= row+ rvect[i];
-        //         int cnext= col+ cvect[i];
-        //         if (rnext>=0 and cnext>=0 and rnext< grid.size() and cnext<grid[0].size() and grid[rnext][cnext]=='1'){
-        //             q.push({rnext, cnext});
-        //         }
-        //     }
-        // }
-        for (int i=0; i<4; i++){
-            int rnext= row+rvect[i];
-            int cnext= col+ cvect[i];
-            if (rnext>=0 and cnext>=0 and rnext<grid.size() and cnext<grid[0].size() and grid[rnext][cnext]=='1'){
-                grid[rnext][cnext]='2';
-                markisland(rnext, cnext, grid);
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        while (q.size()>0){
+            row=q.front().first;
+            col= q.front().second;
+            q.pop();
+            grid[row][col]='2';
+            for(int i=0; i<4; i++){
+                int rnext= row+ rvect[i];
+                int cnext= col+ cvect[i];
+                if (rnext>=0 and cnext>=0 and rnext< grid.size() and cnext<grid[0].size() and grid[rnext][cnext]=='1'){
+                    q.push({rnext, cnext});
+                    grid[rnext][cnext]='2';
+                }
             }
         }
+        // for (int i=0; i<4; i++){
+        //     int rnext= row+rvect[i];
+        //     int cnext= col+ cvect[i];
+        //     if (rnext>=0 and cnext>=0 and rnext<grid.size() and cnext<grid[0].size() and grid[rnext][cnext]=='1'){
+        //         grid[rnext][cnext]='2';
+        //         markisland(rnext, cnext, grid);
+        //     }
+        // }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
