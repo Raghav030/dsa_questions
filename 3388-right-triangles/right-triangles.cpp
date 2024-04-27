d847e0d5c0c215dfa65ac951d1aff0c7d77a8da1
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-04-27 21:33:47 +0530

    Time: 243 ms (62.50%) | Memory: 113.2 MB (25.00%) - LeetSync

---
+++ 3388-right-triangles/right-triangles.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long numberOfRightTriangles(vector<vector<int>>& grid) {
+        long long ans=0;
+        int n= grid.size();
+        int m= grid[0].size();
+//         for (int i=0; i< n-1; i++){
+//             for (int j=0; j<n-1; j++){
+//                 if ((grid[i][j]==1 and (grid[i][j]==grid[i][j+1] and grid[i][j]==grid[i+1][j]) or (grid[i+1][j+1]==1 and grid[i+1][j]== grid[i+1][j+1] and grid[i+1][j]== grid[i][j+1]) or ( grid[i][j]==1 and grid[i+1][j]== grid[i][j] and grid[i][j]== grid[i+1][j+1]) or (grid[i][j]==1 and grid[i][j]==grid[i][j+1] and grid[i][j]== grid[i+1][j+1]))) ans+=1;
+                
+//             }
+            
+//         }
+//         return ans;
+        vector<int > rvect(n, 0);
+        vector<int> cvect(m, 0);
+        for (int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                if (grid[i][j]==1){
+                    rvect[i]+=1;
+                    cvect[j]+=1;
+                    
+                }
+            }
+        }
+        
+        for (int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                long long temp= 1LL* (rvect[i]==0 or cvect[j]==0 )?1LL* 0: 1LL*(rvect[i]-1)*(cvect[j]-1);
+                
+                if (grid[i][j]==1) ans= ans+ temp;
+            }
+        }
+        return ans;
+    }
+};
