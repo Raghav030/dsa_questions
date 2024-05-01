b287bb706abc5dd928a7344bc958583b5b744df5
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-05-01 14:53:18 +0530

    Time: 3 ms (42.21%) | Memory: 7.4 MB (91.39%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -4,7 +4,7 @@ public:
         int n= word.size();
         for (int i=0; i<n; i++){
             if (word[i]==ch){
-                cout<< i;
+                
                 for (int j=0; j<=i/2; j++){
                     swap(word[i-j], word[j]);
                 }
