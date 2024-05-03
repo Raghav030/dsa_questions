29f04d5654a8edc3dd8073342df98ae4ecca73e1
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-05-03 18:56:55 +0530

    Updated Solution - GfG to GitHub

--- Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -8,23 +8,21 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int fknapsack(int w, int n, int wt[], int val[], vector<vector<int>>& dp){
-        if (n<=0) return 0;
-        int take=0;
-        if (dp[n][w]!= -1) return dp[n][w];
-        if (w>= wt[n-1]){
-            take= val[n-1]+ fknapsack(w-wt[n-1], n-1, wt, val, dp);
-        }
-        int nottake= fknapsack(w, n-1, wt, val, dp);
-        return dp[n][w]= max(take, nottake);
-    }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>> dp(n+1, vector<int> (W+1, -1));
-       for (int i=0; i<=n; i++) dp[i][0]=0;
-       for (int i=0; i<=W; i++) dp[0][i]=0;
-       return fknapsack(W, n, wt, val, dp);
+       vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
+       for (int i=1; i<=n; i++){
+           for (int j=1; j<=W; j++){
+               int take=0;
+               if (j>=wt[i-1]){
+                   take= val[i-1]+ dp[i-1][j-wt[i-1]];
+               }
+               int nottake= dp[i-1][j];
+               dp[i][j]=max(take, nottake);
+           }
+       }
+       return dp[n][W];
     }
 };
 
