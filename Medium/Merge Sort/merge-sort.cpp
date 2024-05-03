6ccdea8647f5ee80c5065a12a53acef9e093edf3
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-05-03 12:33:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort/merge-sort.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+        vector<int>vect(r-l+1);
+        int k=0, i=l, j=m+1;
+        while (i<=m and j<=r){
+            if (arr[i]< arr[j]) {
+                vect[k]=arr[i];
+                i+=1;
+            }
+            else {
+                vect[k]=arr[j];
+                j+=1;
+            }
+            k+=1;
+        }
+        while (i<=m){
+            vect[k]=arr[i];
+            i+=1;
+            k+=1;
+        }
+        while (j<=r){
+            vect[k]= arr[j];
+            k+=1;
+            j+=1;
+        }
+        k=0;
+        for (i=l; i<=r; i++){
+            arr[i]=vect[k];
+            k+=1;
+        }
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if (l<r){
+            int mid= (l+ r)/2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
