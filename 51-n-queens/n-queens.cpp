18f760e9b76c5691c5eeb21f55e17f8c66b0c91f
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2024-05-04 07:54:16 +0530

    Time: 33 ms (15.19%) | Memory: 49.1 MB (8.32%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,81 +1,42 @@
 class Solution {
 public:
-    bool checkposition(vector<string>& vect, int row, int col, int n){
-        // for (int i=0; i<n; i++){
-        //     if (vect[row][i]=='Q') return false;
-        // }
-        for (int i=0; i<n; i++){
-            if (vect[i][col]=='Q') return false;
+    bool checkposition(int row, int col, vector<string> grid){
+        for (int i=0; i<grid.size(); i++){
+            if (grid[row][i]=='Q') return false;
+            if (grid[i][col]=='Q') return false;
         }
         int i=row, j=col;
-        while (i>=0 and j>=0 and i<n and j<n){
-            if (vect[i][j]=='Q') return false;
-            i+=1;
-            j-=1;
-        }
-        i=row, j=col;
-        while (i>=0 and j>=0 and i<n and j<n){
-            if (vect[i][j]=='Q') return false;
-            i+=1;
-            j+=1;
-        }
-        i=row, j=col;
-        while (i>=0 and j>=0 and i<n and j<n){
-            if (vect[i][j]=='Q') return false;
+        while (i>=0 and j>=0 and i<grid.size() and j<grid.size()){
+            if (grid[i][j]=='Q') return false;
             i-=1;
             j-=1;
         }
-        i=row, j=col;
-        while (i>=0 and j>=0 and i<n and j<n){
-            if (vect[i][j]=='Q') return false;
+        i=row;
+        j=col;
+        while (i>=0 and j<grid.size() and i<grid.size() and j>=0){
+            if (grid[i][j]=='Q') return false;
             i-=1;
             j+=1;
         }
         return true;
     }
-    void fnqueens(int n, int row, int col, vector<string>& vect, vector<vector<string>>& ans){
+    void fnqueens(int n, int row, vector<string>& grid, vector<vector<string>>& ans){
         if (n==row){
-            ans.push_back(vect);
+            ans.push_back(grid);
             return;
         }
         for (int i=0; i<n; i++){
-            if (checkposition(vect, row, i, n)==true){
-                vect[row][i]='Q';
-                fnqueens(n, row+1,col, vect, ans);
-                vect[row][i]='.';
+            if (checkposition(row, i, grid)==true){
+                grid[row][i]='Q';
+                fnqueens(n, row+1, grid, ans);
             }
+            grid[row][i]='.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
+        vector<string> grid(n, string(n, '.'));
         vector<vector<string>> ans;
-        vector<string> vect(n,string(n, '.'));
-        // for (int i=0; i<n; i++){
-        //     vector<string> vect(n,string(n, '.'));
-        //     // for (int i=0; i<n; i++){
-        //     //     for (int j=0; j<n; j++){
-        //     //         cout<< vect[i]<<' ';
-        //     //     }
-        //     //     cout<< endl;
-        //     // }
-        //     vect[0][i]='Q';
-        //     int flag=0;
-        //     for (int k=1; k<n; k++){
-        //         flag=0;
-        //         for (int j=0; j<n; j++){
-        //             if (checkposition(vect, k, j, n)){
-        //                 vect[k][j]='Q';
-        //                 flag=1;
-        //                 break;
-        //             }
-        //         }
-        //         if (flag==0) break;
-        //     }
-        //     if (flag==1 or n==1){
-        //         ans.push_back(vect);
-        //     }
-        // }
-        fnqueens(n, 0, 0, vect, ans);
+        fnqueens(n, 0, grid, ans);
         return ans;
-        
     }
 };
