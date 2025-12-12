9018120d54e4fafe0fd573a0546a34550ecf7970
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2025-12-12 14:24:50 +0530

    Time: 4 ms (18.54%) | Memory: 101.3 MB (5.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n= prices.size();
-        vector<int> futurevect(n, -1);
-        int ans=0;
-        for (int i= n-1; i>=0; i--){
-            int curprice= prices[i];
-            int futuremaxprice= i!= n-1 ? futurevect[i+1]:-1;
-            futurevect[i]=max(curprice, futuremaxprice);
+        vector<int> suffix_max_vect(n,0);
+        int max_suffix=prices[n-1], max_profit=0, cur_profit=0, cur=0;
+        for (int i=n-1; i>0; i--){
+            cur= prices[i];
+            max_suffix= max(max_suffix, cur);
+            suffix_max_vect[i-1]= max_suffix;
         }
-        for (int i=0; i<n; i++){
-            int curprofit= futurevect[i]-prices[i];
-            ans= max(ans, curprofit);
+        for (int i=0; i<n-1; i++){
+            cur_profit= suffix_max_vect[i]-prices[i];
+            max_profit= max(max_profit, cur_profit);
         }
-        return ans;
+        return max_profit;
     }
 };
