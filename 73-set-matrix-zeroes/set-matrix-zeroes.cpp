cd35c28f9e1c31c4ed4c4c1d560e37df0ce63c18
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2025-12-13 02:44:27 +0530

    Time: 2 ms (12.70%) | Memory: 18.6 MB (50.71%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m= matrix[0].size();
-        set<int> cset;
-        set<int> rset;
+        int n= matrix.size(), m= matrix[0].size();
+        vector<int> rvect(n,1), cvect(m,1);
         for (int i=0; i<n; i++){
             for (int j=0; j<m; j++){
-                if (matrix[i][j] ==0){
-                    rset.insert(i);
-                    cset.insert(j);
+                if (matrix[i][j]==0){
+                    rvect[i]=0;
+                    cvect[j]=0;
                 }
             }
         }
         for (int i=0; i<n; i++){
             for (int j=0; j<m; j++){
-                if (rset.find(i)!= rset.end() or cset.find(j) != cset.end()) matrix[i][j]=0;
+                if (rvect[i]==0 or cvect[j]==0) matrix[i][j]=0;
             }
         }
     }
