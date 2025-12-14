41e15f72b21014f757e62b6e9f359ff2d665cec9
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2025-12-14 13:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (98.84%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        if (numRows==0) return {{}};
-        vector<vector<int>> ans={{1}};
-        // vector<int> tworow={1,1};
-        if (numRows==1) return ans;
-        // if (numRows==2) return tworow;
-        for (int i=2; i<=numRows; i++){
-            vector<int> cur;
-            // if (i==1) cur=onerow;
-            // if(i==2) cur= tworow;
-            cur.push_back(1);
+        vector<vector<int>> ans;
+        for (int i=1; i<=numRows; i++){
+            if (i==1) ans.push_back({1});
+            if (i==2) ans.push_back({1,1});
             if (i>2){
-                for (int j=0; j<ans[i-2].size()-1; j++){
-                    int num= ans[i-2][j]+ans[i-2][j+1];
-                    cur.push_back(num);
+                vector<int> cur(i,1);
+                for(int j=1; j<i-1; j++){
+                    cur[j]=ans[i-2][j]+ans[i-2][j-1];
                 }
+                ans.push_back(cur);
             }
-            cur.push_back(1);
-            ans.push_back(cur);
         }
         return ans;
     }
