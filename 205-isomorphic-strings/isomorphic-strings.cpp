fe4962ed4ee717b02921848dde9759a3064ff583
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2025-12-21 00:08:05 +0530

    Time: 3 ms (40.67%) | Memory: 9.3 MB (78.57%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-       int n=s.size();
-       if (n != t.size()) return false;
-       vector<int> svect(128, -1);
-       vector<int> tvect(128, -1);
-       for (int i=0; i<n; i++){
-        if (svect[s[i]] != -1){
-            if (tvect[t[i]]==-1 or t[svect[s[i]]] != t[i]) return false;
+        // unordered_map <char, int> mpp;
+        vector<int> ascii_nums_st(256,' ');
+        vector<int> ascii_nums_ts(256,' ');
+        char NUL= '\0';
+        int n= s.size(), m=t.size();
+        // if (n != t.size()) return false;
+        for (int i=0; i<max(n,m); i++){
+            // mpp[s[i]]+=1;
+            if (i<n and ascii_nums_st[s[i]-NUL]==' ') ascii_nums_st[s[i]-NUL]=t[i];
+            else if (i<n and ascii_nums_st[s[i]-NUL] != t[i]) {
+                cout<< t[i]<< s[i];
+                return false;
+            }
+            if (i< m and ascii_nums_ts[t[i]-NUL]==' ') ascii_nums_ts[t[i]-NUL]=s[i];
+            else if (i<m and ascii_nums_ts[t[i]-NUL] != s[i]) {
+                cout<<s[i];
+                return false;
+            }
+
         }
-        if (tvect[t[i]] != -1){
-            if (svect[s[i]]==-1 or s[tvect[t[i]]] != s[i]) return false;
-        }
-        svect[s[i]]=i;
-        
-        tvect[t[i]]=i;
-       } 
-   
-       return true;
+        // for (int i=0; i<t.size(); i++){
+        //     if (mpp.find(t[i])==mpp.end() or mpp[t[i]]==0) return false;
+        //     mpp[t[i]]-=1;
+        // }
+        // if (mpp.size()>0) return false;
+        return true;
     }
 };
