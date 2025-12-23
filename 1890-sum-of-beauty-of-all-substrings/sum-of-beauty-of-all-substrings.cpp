cc1eb580d894c77c7ae1e6b6cc77d677ef4bb14f
Raghav Bansal <106468954+Raghav030@users.noreply.github.com>
2025-12-23 10:08:23 +0530

    Time: 1882 ms (5.01%) | Memory: 11.3 MB (52.39%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int beautySum(string s) {
         int n= s.size(), ans=0, cur_frequency=0;
-        vector<int> ascii_arr(26,0);
         for (int i=0; i<n; i++){
-            for (int k=0; k<26; k++) ascii_arr[k]=0;
+            // for (int k=0; k<26; k++) ascii_arr[k]=0;
+            vector<int> ascii_arr(26,0);
             for (int j=i; j<n; j++){
                 ascii_arr[s[j]-'a']+=1;
                 int cur_max=0, cur_min=1e9;
